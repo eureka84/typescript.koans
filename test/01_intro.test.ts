0f3519693b2fc98f00f770d0015c7b67a10aadbb
eureka84 <angelosciarra@ymail.com>
2018-03-11 19:45:17 +0100

    01_intro: replaced functions with lambdas

--- test/01_intro.test.ts
+++ test/01_intro.test.ts
@@ -23,31 +23,31 @@ describe("01_intro", () => {
       expect(_.identity<Object>({})).to.deep.equal({});
     });
 
-    context("when called without arguments", function () {
-      it("should return undefined ", function () {
+    context("when called without arguments", () => {
+      it("should return undefined ", () => {
         expect(_.identity()).to.be.equal(undefined);
       });
     });
   });
 
-  describe("attempt", function () {
-    context("when function throws an error", function () {
-      it("should return error", function () {
+  describe("attempt", () => {
+    context("when function throws an error", () => {
+      it("should return error", () => {
         const err = new Error();
         const func = sinon.stub().throws(err);
         expect(_.attempt(func)).to.be.equal(err);
       });
     });
 
-    context("when function does not throw an error", function () {
-      it("should return result", function () {
+    context("when function does not throw an error", () => {
+      it("should return result", () => {
         const result = 123;
         const func = sinon.stub().returns(result);
         expect(_.attempt(func)).to.be.equal(result);
       });
     });
 
-    it("should apply function with passed in arguments", function () {
+    it("should apply function with passed in arguments", () => {
       const func = sinon.spy();
       _.attempt(func, 1, 2, 3);
       sinon.assert.calledWithExactly(func, 1, 2, 3);
@@ -56,26 +56,26 @@ describe("01_intro", () => {
     });
   });
 
-  describe("noop", function () {
-    it("should return undefined", function () {
+  describe("noop", () => {
+    it("should return undefined", () => {
       expect(_.noop()).to.be.equal(undefined);
     });
 
-    context("when called with arguments", function () {
-      it("should return undefined", function () {
+    context("when called with arguments", () => {
+      it("should return undefined", () => {
         expect(_.noop(1, 2, 3)).to.be.equal(undefined);
       });
     });
   });
 
-  describe("times", function () {
-    it("should invoke iteratee n times", function () {
+  describe("times", () => {
+    it("should invoke iteratee n times", () => {
       const iteratee = sinon.spy();
       _.times<number>(10, iteratee);
       sinon.assert.callCount(iteratee, 10);
     });
 
-    it("should call iteratee with current count", function () {
+    it("should call iteratee with current count", () => {
       const iteratee = sinon.spy();
       _.times<number>(3, iteratee);
       expect(iteratee.getCall(0).args).to.deep.equal([0]);
@@ -83,13 +83,13 @@ describe("01_intro", () => {
       expect(iteratee.getCall(2).args).to.deep.equal([2]);
     });
 
-    it("should return array of iteratee results", function () {
+    it("should return array of iteratee results", () => {
       expect(_.times<number>(3, n => n + 1)).to.deep.equal([1, 2, 3]);
     });
   });
 
-  describe("constant", function () {
-    it("should return a function that returns the supplied value", function () {
+  describe("constant", () => {
+    it("should return a function that returns the supplied value", () => {
       expect(_.constant<number>(1)()).to.be.equal(1);
       expect(_.constant<string>("hello")()).to.be.equal("hello");
     });
