c9e78cc164d66b23ac49b74b7423bbbf39c63608
eureka84 <angelosciarra@ymail.com>
2018-03-14 07:43:00 +0100

    02_array: completed fill, nth and zip

--- src/02_array.ts
+++ src/02_array.ts
@@ -140,7 +140,7 @@ interface DropWhilePredicate<T> {
  *
  */
 export function dropWhile<T>(array: Array<T>, predicate: DropWhilePredicate<T>): Array<T> {
-    if (predicate(head(array))){
+    if (predicate(head(array))) {
         return dropWhile(array.slice(1), predicate);
     }
     return array;
@@ -156,7 +156,7 @@ export function dropWhile<T>(array: Array<T>, predicate: DropWhilePredicate<T>):
  *
  */
 export function dropRightWhile<T>(array: T[], predicate: DropWhilePredicate<T>): T[] {
-    if (predicate(last(array))){
+    if (predicate(last(array))) {
         return dropRightWhile(initial(array), predicate);
     }
     return array;
@@ -170,11 +170,15 @@ export function dropRightWhile<T>(array: T[], predicate: DropWhilePredicate<T>):
  * ## Examples
  * _.fill<any>([4, 6, 8, 10], "* ", 1, 3) => [4, "* ", "* ", 10]
  */
-export function fill() {
+export function fill<T>(array: any[], fillValue: T, start: number, end: number): any[] {
+    return array.map((value: any, index: number) => {
+        return index >= start && index < end ? fillValue : value;
+    });
 }
 
 // Here we define an interface for the predicate used in the findIndex function.
-export interface FindIndexPredicate {
+export interface FindIndexPredicate<T> {
+    (value: T): boolean;
 }
 
 /**
@@ -190,7 +194,17 @@ export interface FindIndexPredicate {
  * _.findIndex([4, 6, 6, 8, 10], value => value === 6, 2) => 2
  *
  */
-export function findIndex() {
+export function findIndex<T>(array: T[], predicate: FindIndexPredicate<T>, start: number = 0): number {
+    function loop<T>(index: number): number {
+        if (index == array.length){
+            return -1;
+        }
+        if (predicate(array[index])){
+            return index;
+        }
+        return loop(index + 1);
+    }
+    return loop(start);
 }
 
 /**
@@ -205,7 +219,17 @@ export function findIndex() {
  * _.findLastIndex([4, 6, 6, 8, 10], value => value === 6, 1) => 1
  *
  */
-export function findLastIndex() {
+export function findLastIndex<T>(array: T[], predicate: FindIndexPredicate<T>, start: number = array.length -1): number {
+    function loop<T>(index: number): number {
+        if (index == 0){
+            return -1;
+        }
+        if (predicate(array[index])){
+            return index;
+        }
+        return loop(index - 1);
+    }
+    return loop(start);
 }
 
 /**
@@ -219,7 +243,8 @@ export function findLastIndex() {
  * _.nth<number>([1, 2, 3]) => 1
  *
  */
-export function nth() {
+export function nth<T>(array: T[], index: number = 0): T {
+    return array[index];
 }
 
 /**
@@ -229,5 +254,6 @@ export function nth() {
  * // We can also use something called "union types" here.
  * _.zip<string | number | boolean>(["a", "b"], [1, 2], [true, false]) => [["a", 1, true], ["b", 2, false]]
  */
-export function zip() {
+export function zip<T>(first: any[], second: any[], third: any[]): T[][] {
+    return first.map((val, index) => [val, second[index], third[index]]);
 }
